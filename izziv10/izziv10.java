e4433674b5b6ea5af0e2890f635143b83102d65d
BlazBone <bb6846@student.uni-lj.si>
2022-05-10 13:09:24 +0200

    end izziv10

--- izziv10/izziv10.java
+++ izziv10/izziv10.java
@@ -1,15 +1,13 @@
-import java.util.Arrays;
 import java.util.Scanner;
 
-/**
- * izziv10
- */
 public class izziv10 {
 
-    private static boolean[][] valid;
-    private static int[][] egg;
-
     public static void printArr(int[][] arr) {
+        System.out.printf("    ");
+        for (int i = 1; i < arr[0].length; i++) {
+            System.out.printf("%4d", i);
+        }
+        System.out.println();
         for (int i = 0; i < arr.length; i++) {
             for (int j = 0; j < arr[i].length; j++) {
                 System.out.printf("%4d", arr[i][j]);
@@ -18,87 +16,86 @@ public class izziv10 {
         }
     }
 
-    static int eggDrop2(int n, int k) {
-        // If there are no floors, then
-        // no trials needed. OR if there
-        // is one floor, one trial needed.
-        if (k == 1 || k == 0)
-            return k;
-
-        // We need k trials for one egg
-        // and k floors
-        if (n == 1)
-            return k;
-
-        int min = Integer.MAX_VALUE;
-        int x, res;
-
-        // Consider all droppings from
-        // 1st floor to kth floor and
-        // return the minimum of these
-        // values plus 1.
-        for (x = 1; x <= k; x++) {
-            res = Math.max(eggDrop(n - 1, x - 1),
-                    eggDrop(n, k - x));
-            if (res < min)
-                min = res;
-        }
-
-        return min + 1;
-    }
-
+    // function for egg dropping problem n number of eggs and k floors
     public static int eggDrop(int n, int k) {
+        int[][] dp = new int[n + 1][k + 1];
         
-        if (valid[n][k]) {
-            return egg[n][k];
+        for (int i = 0; i < dp.length; i++) {
+            dp[i][0] = i;
+            dp[i][1] = i;
         }
-        if (k == 1) {
-            return n;
+        for (int j = 0; j < dp[0].length; j++) {
+            dp[0][j] = 0;
+            dp[1][j] = 1;
+            // dp[1][j] = 1;
         }
-        if (n <= 1) {
-            return 0;
-        }   
-        int min = Integer.MAX_VALUE;
-        for (int i = 1; i <= n; i++) {
-            min = Math.min(min, Math.max(eggDrop(i - 1, k-1), eggDrop(n-i, k)));
-        }
-        valid[n][k]=true;
-        return min + 1;
-
-        // return 1 + Math.min(eggDrop(n - 1, k - 1), eggDrop(n - 1, k));
-        
-    }
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        int k = sc.nextInt();
-        sc.close();
-        valid = new boolean[n+1][k+1];
-        egg = new int[n+1][k+1];
-        int[][] egg2 = new int[n+1][k+1];
-        //egg dropping problem
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < k; j++) {
-                if(j==0){
-                    egg[i][j]=i;
-                }else if(i==0){
-                    egg[i][j]=0;
-                }else{
-                    egg[i][j]=Integer.MAX_VALUE;
+        // printArr(dp);
+        for (int i = 2; i <= n; i++) {
+            for (int j = 2; j <= k; j++) {
+                dp[i][j] = Integer.MAX_VALUE;
+                for (int x = 1; x < i; x++) {
+                    // printf("%d %d %d\n", i, j, x);
+                    // System.out.printf("i: %d j: %d x: %d\n",i,j,x);
+                    dp[i][j] = Math.min(dp[i][j], 1 + Math.max(dp[i-x][j],dp[x - 1][j-1]));
                 }
             }
         }
-        //fill the array egg
-        for (int i = 1; i <= n; i++) {
-            for (int j = 1; j <= k; j++) {
-                egg2[i][j] = eggDrop2(i, j);
-                // egg[i][j] = eggDrop(i, j);
-            }
-        }
-        printArr(egg);
-        printArr(egg2);
-        
-        // egg[i][j] = eggDrop(i, j);
-        // System.out.println(Arrays.deepToString(egg));
+        printArr(dp);
+        return dp[n][k];
     }
+    
+    static int max(int a, int b) {
+        return (a > b) ? a : b;
+    }
+
+    /*
+     * Function to get minimum number of trials needed in worst
+     * case with n eggs and k floors
+     */
+    // static int eggDrop(int n, int k) {
+    //     /*
+    //      * A 2D table where entry eggFloor[i][j] will represent minimum
+    //      * number of trials needed for i eggs and j floors.
+    //      */
+    //     int eggFloor[][] = new int[n + 1][k + 1];
+    //     int res;
+    //     int i, j, x;
+
+    //     // We need one trial for one floor and0 trials for 0 floors
+    //     for (i = 1; i <= n; i++) {
+    //         eggFloor[i][1] = 1;
+    //         eggFloor[i][0] = 0;
+    //     }
+
+    //     // We always need j trials for one egg and j floors.
+    //     for (j = 1; j <= k; j++)
+    //         eggFloor[1][j] = j;
+
+    //     // Fill rest of the entries in table using optimal substructure
+    //     // property
+    //     for (i = 2; i <= n; i++) {
+    //         for (j = 2; j <= k; j++) {
+    //             eggFloor[i][j] = Integer.MAX_VALUE;
+    //             for (x = 1; x <= j; x++) {
+    //                 res = 1 + max(eggFloor[i - 1][x - 1], eggFloor[i][j - x]);
+    //                 if (res < eggFloor[i][j])
+    //                     eggFloor[i][j] = res;
+    //             }
+    //         }
+    //     }
+    //     printArr(eggFloor);
+    //     // eggFloor[n][k] holds the result
+    //     return eggFloor[n][k];
+    // }
+
+    public static void main(String args[]) {
+       Scanner sc = new Scanner(System.in);
+         int n = sc.nextInt();
+         int k = sc.nextInt();
+         sc.close();
+         eggDrop(n, k);
+        
+        
+    }
+    // This code is contributed by Ryuga.
 }
